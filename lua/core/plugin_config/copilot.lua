ffc0619735e7f709122064c1d074ea305b3530ae
mdnihal5 <mdnihal.private1@gmail.com>
2024-11-26 11:42:46 +0530

    removed copilot

--- lua/core/plugin_config/copilot.lua
+++
@@ -1,9 +0,0 @@
--- copilot.lua
-vim.g.copilot_no_tab_map = true  -- Prevent default tab mapping
-
--- Mapping Ctrl-J to accept suggestions
-vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-
--- Optional: Customize suggestion highlight
-vim.cmd([[highlight CopilotSuggestion ctermfg=8 guifg=white guibg=#5c6370]])
-
